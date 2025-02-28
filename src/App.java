50fef00648bc4aa893c3140bd46fa79e26b4302a
sneider <darwinlindarte072@gmail.com>
2025-02-28 13:16:38 -0500

    se sube los primeros ejercicios

--- src/App.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        scanner.close();
-    }
-}
