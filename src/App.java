f764a5c34c6910add1250c9a8a07d5a7cba195df
sneider <darwinlindarte072@gmail.com>
2025-02-28 13:03:22 -0500

    Se arregla estructura con nuevo ejercicio

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
