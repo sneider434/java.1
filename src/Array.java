2c8c15055525c2c4c7d255b84cfc587312e73238
sneider <darwinlindarte072@gmail.com>
2025-03-25 07:40:05 -0500

    se añaden ejercicio de array en clase

---
+++ src/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) { 
+       int[] numeros = new int[5];
+       numeros[0]=10;
+       System.out.println(numeros[0]);
+       for (numeros[0];numeros[5]<5;numeros[0]++){
+        System.out.println(numeros[5]);
+       }
+    }
+}
