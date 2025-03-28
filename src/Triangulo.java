e359868945222af3900fbb965ba0b37737d44e66
Alexis8125 <laarenass@ufpso.edu.co>
2025-03-28 13:53:21 -0500

    wdfefwef

---
+++ src/Triangulo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese numero de coloumnas");
+        int columnas = scanner.nextInt();
+        int filas=0;
+        for (int x =0;x<columnas;x++){
+            filas=filas+1;
+            System.out.println(filas);
+        }
+        String figura = "*";String espacio = "";
+        for (int d = 0;d<columnas;d++){
+            
+            for(int j=columnas;j>filas;j++){
+                System.out.print(figura);
+            }
+            filas=filas+1;
+            System.out.println();
+        }
+       
+
+        scanner.close();
+        }
+}
