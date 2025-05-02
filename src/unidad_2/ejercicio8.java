b77a220980188e3832d6106571eef2966f051479
sneider434 <darwinlindarte072@gmail.com>
2025-05-01 19:36:49 -0500

    mas ejercicios tarea

--- src/unidad_2/ejercicio8.java
+++ src/unidad_2/ejercicio8.java
@@ -6,26 +6,36 @@ public class ejercicio8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[][] alumnos_clase = new String[1000][2];// metriz donde se almacena todo
-        int verificador=1; int contador=0;
+        int contador=0;
         String nombres = "";
         int edad =0;
-        do {
-            contador=contador+1;
-            System.out.println("ingrese nombre del estudiante #"+contador);
+        while (true) {
+            System.out.println("ingrese nombre del estudiante #"+(contador+1));
          nombres= scanner.nextLine();
             if(nombres.contains("*")){
             break;
             }
-            alumnos_clase[contador][1]= nombres;
-             System.out.println(" ingrese la edad de el alumno");
-             edad = scanner.nextInt();
-            alumnos_clase[contador][2]="edad";
+            System.out.println(" ingrese la edad de el alumno #"+(contador+1));
+            edad = scanner.nextInt();
             scanner.nextLine();
-        } while (verificador==1);
-        
-        for(int i=0;i<contador;i++)
-        System.out.println(alumnos_clase[i][2]);
 
+            alumnos_clase[contador][0]= nombres;
+            alumnos_clase[contador][1]=String.valueOf(edad);
+           contador++;
+        }
+        int edadmayor=0;
+        for(int i=0;i<contador;i++){
+            int edad_alumno = Integer.parseInt(alumnos_clase[i][1]);
+            if(edad_alumno>18){
+              System.out.println(" el estudiante "+ alumnos_clase[i][0]+" Es mayor de edad con : "+ alumnos_clase[i][1]);
+            }else{
+                System.out.println(" el estudiante "+ alumnos_clase[i][0]+" No es mayor de edad con : "+ alumnos_clase[i][1]);
+            }
+            if(edad_alumno>edadmayor){
+                edadmayor = edad_alumno;
+            }
+        }
+        System.out.println("el alumno con mayor edad tiene : "+ edadmayor  );
         scanner.close();
     }
 }
---
+++ src/unidad_2/ejercicio9.java
@@ -0,0 +1,10 @@
+package unidad_2;
+
+import java.util.Scanner;
+
+public class ejercicio9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
